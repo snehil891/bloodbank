6983bfd9677fba89362f4c4d5c3b071471b5da92
Shashank Sahay <sahayshashank28@gmail.com>
2022-10-14 14:44:47 +0530

    email

--- project_scripts/send_mail.js
+++ project_scripts/send_mail.js
@@ -8,7 +8,7 @@ function sendEmail_password(email,password) {
       To: email,
       From: "bldbank146@gmail.com",
       Subject: "Forgot Password",
-      Body: "Seems like you forgot your password, well HERE YOU GO!       "+"\n"+"     PASSWORD:"+password,
+      Body: "<html> <p style=\"font-size:14px\">Hi!<br><p style=\"font-size:14px\">You are recieving this E-mail because you or someone else has requested Password recovery from your user account at GITAM BLOODBANK.</p><br><p style=\"font-size:14px\"> The password to login to your account is given below</p><br><p style=\"font-size:25px\">PASSWORD:<strong>"+password+"</p></strong><br>This is an auto-generated E-mail. Hence, do no reply.<br><h2> Best Regards,<br>GITAM BLOODBANK TEAM</h2></html>",
     //   Attachments: [
     //     {
     //       name: "File_Name_with_Extension",
@@ -27,8 +27,8 @@ function sendEmail_username(email,username) {
       Password: "3AECAE4665C09B6618D42B3EB5965F8093DD",
       To: email,
       From: "bldbank146@gmail.com",
-      Subject: "Forgot Password",
-      Body: "Seems like you forgot your username, well HERE YOU GO! %0D%0A USERNAME:"+username,
+      Subject: "Forgot Username",
+      Body: "<html> <p style=\"font-size:14px\">Hi!<br><p style=\"font-size:14px\">You are recieving this E-mail because you or someone else has requested username recovery from your user account at GITAM BLOODBANK.</p><br><p style=\"font-size:14px\"> The username to use at login is given below</p> <br> <p style=\"font-size:25px\">USERNAME:<strong>"+username+"</p></strong><br>This is an auto-generated E-mail. Hence, do no reply.<br><h2> Best Regards,<br>GITAM BLOODBANK TEAM</h2></html>",
     //   Attachments: [
     //     {
     //       name: "File_Name_with_Extension",
