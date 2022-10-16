f9f661457645957f2acc5a733ef9a40a79e360af
snehil891 <snehilsah400@gmail.com>
2022-10-16 15:13:37 +0530

    function redirect

--- project_scripts/page_functionality.js
+++ project_scripts/page_functionality.js
@@ -6,3 +6,16 @@ window.addEventListener("scroll", function () {
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 750);
 });
+function redirect() {
+  let url = "../project_pages/Homepage.html";
+  window.location(url);
+}
+window.history.forward();
+function noBack() {
+  window.history.forward();
+}
+window.history.forward();
+function noForward() {
+  window.history.back();
+}
+
