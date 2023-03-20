0da2c20aff9dce807ab3ab14f4a1185a54f778e3
snehil891 <snehilsah400@gmail.com>
2023-03-20 12:33:45 +0530

    Form layout changed

--- project_scripts/donation_db.js
+++ project_scripts/donation_db.js
@@ -38,7 +38,6 @@ function get_value_gender() {
     return "Others";
   }
 }
-
 function get_donation() {
   console.log("getting");
   // const donator_name_field = document.getElementById("Donator_name");
@@ -51,12 +50,31 @@ function get_donation() {
   else{
     document.getElementById("donator_name_error").innerHTML="";
   }
+  if (donator_name.includes(1) || donator_name.includes(2) || donator_name.includes(3) || donator_name.includes(4) || donator_name.includes(5) || donator_name.includes(6) || donator_name.includes(7) || donator_name.includes(8) || donator_name.includes(9) || donator_name.includes(0) ){
+    document.getElementById("donator_name_error").innerHTML="Enter Valid  name.";
+    document.getElementById("donator_name_error").style.color = "red";;
+    return false;
+  }
+
+  else{
+    document.getElementById("donator_name_error").innerHTML="";
+  }
   const donator_email = document.getElementById("Donator_email").value;
-  if (!((donator_email.includes("@") && donator_email.includes(".in"))||(donator_email.includes("@") && donator_email.includes(".com"))||(donator_email.includes("@") && donator_email.includes(".edu")))){  
+
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!donator_email.match(validRegex))
+   {
     document.getElementById("donator_email_error").innerHTML="Enter a valid E-mail.";
     document.getElementById("donator_email_error").style.color = "red";;
     return false;
-  }
+   }
+    if (!((donator_email.includes("@") && donator_email.includes(".in"))||(donator_email.includes("@") && donator_email.includes(".com"))||(donator_email.includes("@") && donator_email.includes(".edu")))){  
+      document.getElementById("donator_email_error").innerHTML="Enter a valid E-mail.";
+      document.getElementById("donator_email_error").style.color = "red";;
+      return false;
+    }
+  
+   
   else{
     document.getElementById("donator_email_error").innerHTML="";
   }
@@ -66,8 +84,34 @@ function get_donation() {
     document.getElementById("donator_phone_error").style.color = "red";;
     return false;
   }
+  else if(donator_phone[0]==="1" || donator_phone[0]==="2" || donator_phone[0]==="3" || donator_phone[0]==="4" || donator_phone[0]==="5"|| donator_phone[0]==="0"){
+    document.getElementById("donator_phone_error").innerHTML="Enter a valid phone number.";
+    document.getElementById("donator_phone_error").style.color = "red";;
+    return false;
+  }
   else{
+    console.log(donator_phone[1])
     document.getElementById("donator_phone_error").innerHTML="";
+  }
+  const donator_blood = document.getElementById("Blood_group").value;
+  // const donator_blood = select.options[select.selectedIndex].value;
+  console.log(donator_blood);
+  if(donator_blood==="Enter Blood Group"){
+    document.getElementById("donator_blood_error").innerHTML="Kindly select blood group.";
+    document.getElementById("donator_blood_error").style.color = "red";;
+    return false;
+  }
+  else{
+    document.getElementById("donator_blood_error").innerHTML="";
+  }
+  const donator_gender = document.getElementById("Gender").value;
+  if(donator_gender ==="Enter Gender"){
+    document.getElementById("donator_gender_error").innerHTML="Select a gender.";
+    document.getElementById("donator_gender_error").style.color = "red";;
+    return false;
+  }
+  else{
+    document.getElementById("donator_gender_error").innerHTML="";
   }
   const donator_dob = new Date(document.getElementById("Donator_dob").value);
   if(donator_dob=="Invalid Date"){
@@ -78,17 +122,7 @@ function get_donation() {
   else{
     document.getElementById("donator_dob_error").innerHTML="";
   }
-  const donator_blood = document.getElementById("Blood_group").value;
-  // const donator_blood = select.options[select.selectedIndex].value;
-  console.log(donator_blood);
-  if(!donator_blood){
-    document.getElementById("donator_blood_error").innerHTML="Kindly select blood group.";
-    document.getElementById("donator_blood_error").style.color = "red";;
-    return false;
-  }
-  else{
-    document.getElementById("donator_blood_error").innerHTML="";
-  }
+ 
   const donator_address = document.getElementById("Donator_address").value;
   if(donator_address.length==0){
     document.getElementById("donator_address_error").innerHTML="Kindly fill the address.";
@@ -98,15 +132,7 @@ function get_donation() {
   else{
     document.getElementById("donator_address_error").innerHTML="";
   }
-  const donator_gender = get_value_gender();
-  if(!donator_gender){
-    document.getElementById("donator_gender_error").innerHTML="Select a gender.";
-    document.getElementById("donator_gender_error").style.color = "red";;
-    return false;
-  }
-  else{
-    document.getElementById("donator_gender_error").innerHTML="";
-  }
+
   
   
   
@@ -115,28 +141,11 @@ function get_donation() {
     donator_dob,
     donator_gender,
     donator_email,
-    donator_address,
     donator_blood,
-    donator_phone
+    donator_phone,
+    donator_address
   ];
 }
-
-// async function donate_check(phone) {
-//   var flag = 0;
-//   const querySnapshot = await getDocs(collection(db, "Donate"));
-//   querySnapshot.forEach((doc) => {
-//     if (doc.id === phone) {
-//       flag = 1;
-//     }
-//   });
-//   if (flag === 1) {
-//     console.log("previous record found");
-//     return false;
-//   } else {
-//     console.log("No Username");
-//     return true;
-//   }
-// }
 
 async function check_age(dob) {
   console.log("checking");
@@ -160,63 +169,6 @@ async function add_to_donation() {
   if(!donation_data){
     return 0;
   }
-  // let error_string = ["\n"];
-  // if (flag == 0) {
-  //   donation_data.forEach((element) => {
-  //     if (element == "Invalid Date") {
-  //       if(!error_string.includes("DOB is not filled properly.")){
-  //         error_string.push("DOB is not filled properly.");
-  //         error_string.push("\n");}
-  //       flag=1;
-  //       // return 0;
-
-  //     } else if (!element) {
-  //       if(!error_string.includes("All fields are not filled properly or left empty.")){
-  //         error_string.push("All fields are not filled properly or left empty.");
-  //         error_string.push("\n");
-  //     }
-  //         flag=1;
-  //   // return 0;
-  //     } else if (element.length == 0) {
-  //       if(!error_string.includes("Some fields are empty.")){
-  //       error_string.push("Some fields are empty.");
-  //       error_string.push("\n");}
-  //       flag=1;
-  //   // return 0;
-  //     }
-  //   });
-  // }
-  // if(donation_data[6].length!=10){
-  //   error_string.push("Invalid phone number.");
-  //   error_string.push("\n");
-  //   flag=1;
-  //   // return 0;
-  // }
-  // // if (!(donator_email.includes("@gmail.com") || donator_email.includes("@gitam.in") ||donator_email.includes("@hotmail.com")||donator_email.includes("@gitam.edu")||donator_email.includes("@yahoo.com"))) {
-  // if (!((donator_email.includes("@") && donator_email.includes(".in"))||(donator_email.includes("@") && donator_email.includes(".com"))||(donator_email.includes("@") && donator_email.includes(".edu")))){  
-  //   error_string.push("Invalid E-mail.");
-  //   error_string.push("\n");
-  //   flag=1;
-  //   // return 0;
-  // }  
-  // if (flag == 1) {
-  //   let error_message = "";
-  //   for (let i = 0; i < error_string.length-1; i++) {
-  //     const element = error_string[i];      
-  //     error_message+=element;
-  //     if(element=="\n"){
-  //       // error_message+=element;
-  //       error_message+="•  ";
-  //     }
-  //   }
-  //   // error_string.forEach((element) => {
-      
-  //   //   error_message+=element;
-      
-  //   // });
-  //   alert("Can't proceed due to the following reasons:"+error_message);
-  //   return 0;
-  // }
   const time = String(new Date());
   console.log(time);
   const dbRef = collection(db, "Donate");
@@ -269,9 +221,9 @@ async function add_to_donation() {
       .then(async (docRef) => {
         console.log("Document has been added successfully");
         await sendemail_donation(donation_data[3]);
-
-        alert("Thank you for Donation. Redirecting to Home.");
-        setTimeout(myURL, 100);
+        window.location="#sec-2";
+        document.getElementById("sec-2").style.display = "block";
+        setTimeout(myURL, 7000);
         function myURL() {
           location.href = "Homepage.html";
         }
--- project_scripts/request_db.js
+++ project_scripts/request_db.js
@@ -34,17 +34,6 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 const db = getFirestore();
 
-function get_value_gender() {
-  if (document.getElementById("dot-1").value) {
-    return "male";
-  }
-  if (document.getElementById("dot-2").value) {
-    return "female";
-  }
-  if (document.getElementById("dot-3").value) {
-    return "others";
-  }
-}
 
 function get_request() {
   console.log("getting");
@@ -88,6 +77,17 @@ function get_request() {
   }
   else{
     document.getElementById("patient_phone_error").innerHTML="";
+  }
+
+  const patient_blood = document.getElementById("PBlood_group_request").value;
+  if(patient_blood==="Enter Blood Group"){
+    document.getElementById("patient_blood_error").innerHTML="Kindly select blood group.";
+    document.getElementById("patient_blood_error").style.color = "red";
+    return false;
+  }
+  else{
+
+    document.getElementById("patient_blood_error").innerHTML="";
   }
   const patient_required_date = new Date(
     document.getElementById("required_date").value
@@ -135,44 +135,8 @@ function get_request() {
   else{
     document.getElementById("patient_units_error").innerHTML="";
   }
-  // const Hospital_name = document.getElementById("_name").value;
-  // if(_name.length==0){
-  //   document.getElementById("patient__error").innerHTML="Kindly fill the  name.";
-  //   document.getElementById("patient__error").style.color = "red";
-  //   return false;
-  // }
-  // else{
-  //   document.getElementById("patient__error").innerHTML="";
-  // }
-  
-  // const purpose_request = document.getElementById("purpose_request").value;
-  // if(purpose_request.length==0){
-  //   document.getElementById("patient_purpose_error").innerHTML="Kindly provide purpose.";
-  //   document.getElementById("patient_purpose_error").style.color = "red";
-  //   return false;
-  // }
-  // else{
-  //   document.getElementById("patient_purpose_error").innerHTML="";
-  // }
-  const patient_blood = document.getElementById("Blood_group_request").value;
-  const blood_grp = ["A+","B+","O+","AB+","A-","B-","O-","AB-","Others"]
-  // const patient_blood = select.options[select.selectedIndex].value;
-  if(!patient_blood){
-    document.getElementById("patient_blood_error").innerHTML="Kindly select blood group.";
-    document.getElementById("patient_blood_error").style.color = "red";
-    return false;
-  }
-  else if(!blood_grp.includes(patient_blood)){
-    document.getElementById("patient_blood_error").innerHTML="Kindly select a valid blood group.";
-    document.getElementById("patient_blood_error").style.color = "red";
-    return false;  
-  }
-  else{
-
-    document.getElementById("patient_blood_error").innerHTML="";
-  }
-  const patient_gender = get_value_gender();
-  if(!patient_gender){
+  const patient_gender = document.getElementById("PGender").value;
+  if(patient_gender ==="Enter Gender"){
     document.getElementById("patient_gender_error").innerHTML="Kindly select gender.";
     document.getElementById("patient_gender_error").style.color = "red";;
     return false;
@@ -181,17 +145,39 @@ function get_request() {
     document.getElementById("patient_gender_error").innerHTML="";
   }
   
+  const purpose_request = document.getElementById("purpose_request").value;
+  if(purpose_request.length==0){
+    document.getElementById("patient_purpose_error").innerHTML="Kindly provide purpose.";
+    document.getElementById("patient_purpose_error").style.color = "red";
+    return false;
+  }
+  else{
+    document.getElementById("patient_purpose_error").innerHTML="";
+  }
+
+  const Hospital_name = document.getElementById("Hospital_name").value;
+  if(Hospital_name.length==0){
+    document.getElementById("patient_hospital_error").innerHTML="Kindly fill the hospital name.";
+    document.getElementById("patient_hospital_error").style.color = "red";
+    return false;
+  }
+  else{
+    document.getElementById("patient_hospital_error").innerHTML="";
+  }
+  
+  
+ 
   return [
     no_of_units, //1
     patient_name, //2
-    // Hospital_name,//3
-    patient_age,//3
-    patient_gender,//4
-    patient_email,//5
-    // purpose_request,//6
-    patient_blood,//6
-    patient_required_date,//7
-    patient_phone//8
+    Hospital_name,//3
+    patient_age,//4
+    patient_gender,//5
+    patient_email,//6
+    purpose_request,//7
+    patient_blood,//8
+    patient_required_date,//9
+    patient_phone//10
   ];
 }
 
@@ -261,16 +247,16 @@ async function add_to_request() {
     ),
     {
       Name: request_data[1],
-      // Hospital_Name: request_data[2],
-      age: request_data[2],
-      required_date: String(request_data[6]),
-      Gender: request_data[3],
-      Email: request_data[4],
-      // Purpose: request_data[5],
-      Blood_group: request_data[5],
+      Hospital_Name: request_data[2],
+      age: request_data[3],
+      required_date: String(request_data[8]),
+      Gender: request_data[4],
+      Email: request_data[5],
+      Purpose: request_data[6],
+      Blood_group: request_data[7],
       Date_of_creation: time,
       No_of_Units: request_data[0],
-      Phone:request_data[7]
+      Phone:request_data[9]
     }
   )
     .then(async (docRef) => {
